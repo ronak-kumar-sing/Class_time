8b938cc113fa85ada279a0a4ccfee93206fee084
Ronak Kumar <ronakkumarsingh@Ronaks-MacBook-Air-2.local>
2025-02-05 13:33:32 +0530

    first commit

---
+++ java.java
@@ -0,0 +1,12 @@
+class java {
+    public static void main(String[] args) {
+        String str = "Hello";
+        String str2 = new String("World!");
+
+        String lit2 = "World!";
+        String lit3 = "Hello";
+        System.out.println(str + " " + str2);
+        System.out.println(str == lit3);
+
+    }
+}

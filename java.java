5077c18b4cffc1a1cbf79afa0e830dc4a8a76f7c
ronak-kumar-sing <ronakkumarsingh23@lpu.in>
2025-02-11 16:31:27 +0530

    Secand commit

--- java.java
+++ java.java
@@ -1,16 +1,38 @@
-class java {
+class Stringexample {
     public static void main(String[] args) {
-        String str = "Hello";
-        String str2 = new String("World!");
+        String l1 = "I love java";
+        String s1 = new String("I love java");
+        String l2 = "I love java";
+        String s2 = new String("I love java");
 
-        String lit2 = "World!";
-        String lit3 = "Hello";
-        System.out.println(str + " " + str2);
-        System.out.println(str == lit3);
+        System.out.println("Though literals 1 " + l1);
+        System.out.println("Through new keyword 1 " +s1) ;
+        System.out.println("Though literals 2 " +l2) ;
+        System.out.println("Through new keyword 2 " + s2);
 
-        byte b = { 127, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110 };
-        System.out.println(b[0]);
+        System.out.println("l1 == s1 : " + (l1 == s1));
+        System.out.println("l1 == l2 : " + (l1 == l2));
+        System.out.println("s1 == s2 : " + (s1 == s2)); // two different objects
+        System.out.println("l1.equals(s1) : " + l1.equals(s1));
+        System.out.println("l1.equals(l2) : " + l1.equals(l2));// contents of two objects are same
+        System.out.println("Tim".equals("tim")); // false
+        System.out.println("I love java".equals(s1)); // true
 
+
+        System.out.println(s1.charAt(3));
+        System.out.println(s1.indexOf('v'));
+        System.out.println(s1.lastIndexOf('v'));
+
+        System.out.println(s1.substring(2, 6));
+
+        System.out.println(s1.replace('v', 'V'));
+
+        String s3 = new String(s1.toUpperCase());
+
+        System.out.println(s3);
+
+        String replace = s1.replace("love", "hate");
+        System.out.println(replace);
 
     }
 }
